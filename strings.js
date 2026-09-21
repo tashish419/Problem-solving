680fab730e06e400fd00afc123da22c5594a77d3
tashish419 <tashish410@gmail.com>
2026-09-21 23:05:15 +0530

    valid anagram

--- strings.js
+++ strings.js
@@ -12,3 +12,30 @@ function charFrequncy(str) {
 
     return hashMap;
 }
+
+//Q2 - Valid Anagram: Return true if str2 is an anagram of str1.
+function isAnangram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    let hashMap = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        if (hashMap[str1[i]]) {
+            hashMap[str1[i]]++;
+        } else {
+            hashMap[str1[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < str2.length; i++) {
+        if (hashMap[str2[i]]) {
+            hashMap[str2[i]]--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
