7a6d8942211ba6c0bc054fe6c65abcf61e113c21
tashish419 <tashish410@gmail.com>
2026-09-22 18:21:24 +0530

    simple palindrome check

--- strings.js
+++ strings.js
@@ -42,22 +42,36 @@ function isAnangram(str1, str2) {
 
 //Q3 -------> First Non-Repeating Character
 function firstUniqueChar(str) {
-    let obj = {}
+    let obj = {};
 
     for (let i = 0; i < str.length; i++) {
-
-        if(obj[str[i]]){
-            obj[str[i]]++
-        }else{
-            obj[str[i]] = 1
+        if (obj[str[i]]) {
+            obj[str[i]]++;
+        } else {
+            obj[str[i]] = 1;
         }
     }
 
     for (let i = 0; i < str.length; i++) {
-        if(obj[str[i]] === 1){
-            return i
+        if (obj[str[i]] === 1) {
+            return i;
         }
     }
 
-    return -1
+    return -1;
+}
+
+//Q4 ----> Simple Plaindrome check
+function palindromeCheck(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
