d2a4900ce98d830037b2d387d057cd73f7007628
tashish419 <tashish410@gmail.com>
2026-09-26 20:26:08 +0530

    Armstrong number

--- Basic-maths/index.js
+++ Basic-maths/index.js
@@ -73,3 +73,23 @@ function factorial(n) {
     }
     return fact;
 }
+
+//Q7 -------- Check Armstrong number
+function isArmstrong(n) {
+    let original = n;
+    let count = 0;
+    let sum = 0;
+    while (n > 0) {
+        n = Math.floor(n / 10);
+        count++;
+    }
+    n = original;
+    while (n > 0) {
+        let lastDigit = n % 10;
+        n = Math.floor(n / 10);
+
+        sum = sum + lastDigit ** count;
+    }
+
+    return sum === original;
+}
