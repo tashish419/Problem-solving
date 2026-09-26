261bc828de688df30de04db46402ec5f9e719483
tashish419 <tashish410@gmail.com>
2026-09-26 22:02:09 +0530

    perfect number

--- Basic-maths/index.js
+++ Basic-maths/index.js
@@ -93,3 +93,14 @@ function isArmstrong(n) {
 
     return sum === original;
 }
+
+//Q8 ------ check perfect number
+var checkPerfectNumber = function(num) {
+    let sum = 0
+    for(let i = 1; i < num; i++){
+        if(num%i === 0){
+            sum = sum + i
+        }
+    }
+    return sum === num;
+};
